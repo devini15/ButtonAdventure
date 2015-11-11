02cd8e6ca1735deffdf43616df1960a6d4d916b1
student3 <student3@jh-mbair-158-12.d214.org>
2015-11-11 14:32:15 -0600

    Initial Commit

---
+++ Press Adventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Press Adventure
+//
+//  Created by student3 on 11/11/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
