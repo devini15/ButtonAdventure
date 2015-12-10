1cba2de1a45be168e08ae2351b17a8bba863c5a0
student3 <student3@jh-mbair-158-12.d214.org>
2015-12-10 14:09:28 -0600

    Up to first choice.

---
+++ Press Adventure/NameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NameViewController.swift
+//  Press Adventure
+//
+//  Created by student3 on 11/11/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    @IBOutlet weak var playerTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Enter Name"
+
+       let name = playerTextField.text
+
+
+        
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
+
+class Name
+{
+}
